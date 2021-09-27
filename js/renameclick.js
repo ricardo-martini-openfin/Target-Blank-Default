d336b975618083e0b6368e4d2e581c152aa13fe7
Ricardo Martini <ricardo.martini@openfin.co>
2021-09-27 14:31:07 -0400

    changed public json

--- js/renameclick.js
+++ js/renameclick.js
@@ -19,27 +19,57 @@
 // }
 
 
-fin.View.addEventListener("created", function(event) {
-    stopNavigation()
-    let viewOptions = {
-        url: '',
-        printName: 'News',
-        processAffinity: 'mw_1'
-    }
+// fin.View.addEventListener("created", function(event) {
+//     stopNavigation()
+//     let viewOptions = {
+//         url: '',
+//         printName: 'News',
+//         processAffinity: 'mw_1'
+//     }
         
     
-    fin.Platform.getCurrentSync().createView(viewOptions, fin.me.identity)
-    // let a = document.getElementsByTagName('a');
-    // for (let i=0; i<a.length; i++){
-    //     a[i].setAttribute('target', '_blank');
-    // }
+//     fin.Platform.getCurrentSync().createView(viewOptions, fin.me.identity)
+//     // let a = document.getElementsByTagName('a');
+//     // for (let i=0; i<a.length; i++){
+//     //     a[i].setAttribute('target', '_blank');
+//     // }
+// }
+// );
+
+// async function click_me(event) {
+//     // let a = document.getElementsByTagName('a');
+//     // for (let i=0; i<a.length; i++){
+//     //     a[i].setAttribute('target', '_blank');
+//     // }
+//     // stop the browser automatically opening a new window
+//     event.preventDefault();
+//     // alert('here');
+//     // window.location = event.target.href;
+//     // do whatever you want with the url
+//     const { identity } = await fin.me.getCurrentWindow();
+//     const url = event.target.href;
+//     fin.Platform.getCurrentSync().createView({url}, identity);
+// }
+
+// async function stopNavigation() {
+//     const view = await fin.View.getCurrent()
+//     return await view.stopNavigation();
+// }
+
+
+let a_elements = Array.from(document.getElementsByTagName('a'));
+
+console.log(a_elements)
+a_elements.forEach(addEventListener("click", click_me))
+
+
+async function click_me(event) {
+    // stop the browser automatically navigating
+    event.preventDefault();
+    // alert('here');
+    // window.location = event.target.href;
+    // do whatever you want with the url
+    const { identity } = await fin.me.getCurrentWindow();
+    const url = event.target.href;
+    fin.Platform.getCurrentSync().createView({url}, identity);
 }
-);
-
-
-async function stopNavigation() {
-    const view = await fin.View.getCurrent()
-    return await view.stopNavigation();
-}
-
-
